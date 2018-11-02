aa92cd28e87e0d94eef930aac150d55ce6ed4805
calwulf@wl.k12.ia.us <calwulf@wl.k12.ia.us>
2018-11-02 15:24:35 -0500

    downloaded jquery and handlebars

--- js/meme-constructor.js
+++ js/meme-constructor.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var meme_list = [];
+
+var Meme = function(meme){
+    this.image = product.image;
+    this.title = product.title;
+    this.category = product.category;
+    this.easterEgg = product.easterEgg;
+    this.meme_id = product.meme_id;
+
+};
+Meme.prototype.toHtml = function(){
+    var source = $('#meme-template').text();
+    var template = Handlebars.compile(source);
+    return template(this);
+};
+
+memes.forEach(function(meme){
+    meme_list.push(new Meme(meme));
+});
+meme_list.forEach(function(meme){
+    $('#meme-listings').append(meme.toHtml());
+});
